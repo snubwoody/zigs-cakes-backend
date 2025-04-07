de347b0d54a8f3fb01933cdfde8b933cc5d48d1a
waku <wakunguma13@gmail.com>
2025-04-07 16:52:58 -0600

    feat: added delete and create flavour endpoints to the router

--- server-core/src/admin/mod.rs
+++ server-core/src/admin/mod.rs
@@ -14,6 +14,8 @@ pub fn admin_api(state: AppState) -> Router<AppState>{
         .route("/order/{id}/status/{status}", patch(update_order_status))
         .route("/cart/{id}/items", get(fetch_cakes))
 		.route("/cakes/flavor/{id}", patch(update_flavor))
+		.route("/cakes/flavor", post(create_flavor))
+		.route("/cakes/flavor", delete(delete_flavor))
         .layer(axum::middleware::from_fn_with_state(
             state.clone(),
             auth_middleware,
