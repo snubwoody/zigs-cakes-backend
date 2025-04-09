d52e5dc82d48e7dab3ad5e0fbd19f0e9c38509af
waku <wakunguma13@gmail.com>
2025-04-08 19:23:24 -0600

    refactor:  moved the size endpoints to the admin module

--- server-core/src/admin/mod.rs
+++ server-core/src/admin/mod.rs
@@ -1,13 +1,19 @@
 mod flavor;
-mod size;
 pub use flavor::*;
-pub use size::*;
 use crate::{AppState,middleware::auth_middleware};
 use axum::{
     Router,
     routing::{delete, get, patch, post},
 };
 use crate::api::v1::orders::*;
+use crate::{db::CakeSize, services::size::{SizeService, UpdateSizeDesc}, 
+ErrorResponse};
+use axum::{
+    extract::{Path, State}, http::StatusCode, Json
+};
+use rust_decimal::Decimal;
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
 
 pub fn admin_api(state: AppState) -> Router<AppState>{
 	Router::new()
@@ -22,4 +28,72 @@ pub fn admin_api(state: AppState) -> Router<AppState>{
             state.clone(),
             auth_middleware,
         ))
+}
+
+
+/// The payload used when creating a size
+#[derive(Serialize, Deserialize, ToSchema)]
+pub struct CreateSizePayload {
+    pub inches: i32,
+    pub layers: i32,
+    pub price: Decimal,
+}
+
+
+#[utoipa::path(
+	post,
+	path="/api/v2/admin/cakes/size",
+	responses(
+		(status=201,description="Cake size created successfully",body=CakeSize),
+		(status=401,description="Missing authorization header",body=ErrorResponse)
+	)
+)]
+/// Create a new size
+pub async fn create_size(
+    State(state): State<AppState>,
+    Json(payload): Json<CreateSizePayload>,
+) -> crate::Result<(StatusCode,Json<CakeSize>)> {
+	let sizes = SizeService::new(state.clone());
+	let CreateSizePayload { inches, layers, price } = payload;
+	let size = sizes.create(inches, layers, price).await?;
+	
+	Ok((
+		StatusCode::CREATED,
+		Json(size)
+	))
+}
+
+#[utoipa::path(
+	patch,
+	path="/api/v2/admin/cakes/size/{id}",
+	responses(
+		(status=200,description="Flavour created successfully"),
+		(status=401,description="Missing authorization header",body=ErrorResponse)
+	)
+)]
+/// Update a size
+pub async fn update_size(
+    State(state): State<AppState>,
+    Path(id): Path<i32>,
+    Json(desc): Json<UpdateSizeDesc>,
+) -> crate::Result<()> {
+	let sizes = SizeService::new(state.clone());
+	sizes.update(id,desc).await
+}
+
+#[utoipa::path(
+	delete,
+	path="/api/v2/admin/cakes/sizes/{id}",
+	responses(
+		(status=200,description="Flavour deleted successfully"),
+		(status=401,description="Missing authorization header",body=ErrorResponse)
+	)
+)]
+/// Delete a [`CakeSize`]
+pub async fn delete_size(
+    State(state): State<AppState>,
+    Path(id): Path<i32>,
+) -> crate::Result<()> {
+	let sizes = SizeService::new(state.clone());
+	sizes.delete(id).await
 }
